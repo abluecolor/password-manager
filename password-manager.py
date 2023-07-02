7443bb50dbe34c938d47970a3103a5d2d9a719af
Maylen <maylendeulofeu@gmail.com>
2023-07-02 16:50:38 -0400

    Change master pwd variable and add new pwd variable

--- password-manager.py
+++ password-manager.py
@@ -1,11 +1,12 @@
-pwd = input("What is the master password? ")
+master_pwd = input("What is the master password? ")
 
 def view():
     pass
 
 
 def add():
-    pass
+    name = input("Account Name: ")
+    pwd = input("Password ")
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press Q to quit?? ").lower()
