8b5db740e00bd7bf4d897521c15df41cb2ecb63a
Maylen <maylendeulofeu@gmail.com>
2023-07-02 18:19:05 -0400

    Fix decoding bug

--- password-manager.py
+++ password-manager.py
@@ -21,14 +21,14 @@ def view():
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:", user, "| Password:", str(fer.decrypt(passw.encode())))
+            print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
     with open("passwords.txt", "a") as f:
-        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press Q to quit? ").lower()
