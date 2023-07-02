1e8d28d40f34c5516b6c98ccfe15ef3ebe4d2469
Maylen <maylendeulofeu@gmail.com>
2023-07-02 17:01:22 -0400

    Write user and pass on new txt and fix typos

--- password-manager.py
+++ password-manager.py
@@ -6,12 +6,13 @@ def view():
 
 def add():
     name = input("Account Name: ")
-    pwd = input("Password ")
+    pwd = input("Password: ")
 
     with open("passwords.txt", "a") as f:
+        f.write(name + "|" + pwd)
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones (view, add), press Q to quit?? ").lower()
+    mode = input("Would you like to add a new password or view existing ones (view, add), press Q to quit? ").lower()
     if mode == "q":
         break
 
