047a5541bd5538c35f2a716cc8969059347e73e7
Maylen <maylendeulofeu@gmail.com>
2023-07-02 17:55:27 -0400

    Comment out key generator to avoid issues

--- password-manager.py
+++ password-manager.py
@@ -2,18 +2,18 @@ from cryptography.fernet import Fernet
 
 master_pwd = input("What is the master password? ")
 
+'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)
-    
+        key_file.write(key)'''
+
 def view():
     with open("passwords.txt", "r") as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
             print("User:", user, "| Password:", passw)
-
 
 def add():
     name = input("Account Name: ")
