61154c77ccef1b3066062207f5247b2a2150ab17
Maylen <maylendeulofeu@gmail.com>
2023-07-02 16:48:47 -0400

    Declare and call view() and add()

--- password-manager.py
+++ password-manager.py
@@ -1,14 +1,21 @@
 pwd = input("What is the master password? ")
 
+def view():
+    pass
+
+
+def add():
+    pass
+
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press Q to quit?? ").lower()
     if mode == "q":
         break
 
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
-        pass
+        add()
     else:
         print("Invalid mode.")
         continue
